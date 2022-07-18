eeaed8f31e75b1240b5f147f0fe608eab649fb1c
Jean <jean.silva@dbccompany.com.br>
2022-07-17 21:06:44 -0300

    documentando

--- src/main/java/br/com/dbccompany/time7/gestaodeensino/dto/NotaCreateDTO.java
+++ src/main/java/br/com/dbccompany/time7/gestaodeensino/dto/NotaCreateDTO.java
@@ -1,4 +1,5 @@
 package br.com.dbccompany.time7.gestaodeensino.dto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,8 +9,15 @@ import lombok.*;
 @ToString
 public class NotaCreateDTO {
 
+    @Schema(description = "Nota 1 do Aluno")
     private Double nota1;
+
+    @Schema(description = "Nota 1 do Aluno")
     private Double nota2;
+
+    @Schema(description = "Nota 1 do Aluno")
     private Double nota3;
+
+    @Schema(description = "Nota 1 do Aluno")
     private Double nota4;
 }
--- src/main/java/br/com/dbccompany/time7/gestaodeensino/dto/NotaDTO.java
+++ src/main/java/br/com/dbccompany/time7/gestaodeensino/dto/NotaDTO.java
@@ -1,5 +1,6 @@
 package br.com.dbccompany.time7.gestaodeensino.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -9,12 +10,27 @@ import lombok.*;
 @ToString
 public class NotaDTO {
 
+    @Schema(description = "ID único que relaciona uma disciplina e um aluno")
     private Integer idNota;
+
+    @Schema(description = "ID da disciplina relacionada à nota")
     private Integer idDisciplina;
+
+    @Schema(description = "ID do aluno relacionado à nota")
     private Integer idAluno;
+
+    @Schema(description = "Nota 1 do aluno, referente à disciplina")
     private Double nota1;
+
+    @Schema(description = "Nota 1 do aluno, referente à disciplina")
     private Double nota2;
+
+    @Schema(description = "Nota 1 do aluno, referente à disciplina")
     private Double nota3;
+
+    @Schema(description = "Nota 1 do aluno, referente à disciplina")
     private Double nota4;
+
+    @Schema(description = "Nota 1 do aluno, referente à disciplina")
     private Double media;
 }
