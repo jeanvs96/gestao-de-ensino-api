6adce3ede9e40cf37216891e467e8c2cac9d762e
Jean <jean.silva@dbccompany.com.br>
2022-07-17 22:02:03 -0300

    General Responses

--- src/main/java/br/com/dbccompany/time7/gestaodeensino/controller/NotaController.java
+++ src/main/java/br/com/dbccompany/time7/gestaodeensino/controller/NotaController.java
@@ -3,6 +3,7 @@ package br.com.dbccompany.time7.gestaodeensino.controller;
 import br.com.dbccompany.time7.gestaodeensino.dto.NotaCreateDTO;
 import br.com.dbccompany.time7.gestaodeensino.dto.NotaDTO;
 import br.com.dbccompany.time7.gestaodeensino.exceptions.RegraDeNegocioException;
+import br.com.dbccompany.time7.gestaodeensino.response.Response;
 import br.com.dbccompany.time7.gestaodeensino.service.NotaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -25,26 +26,14 @@ public class NotaController {
     private final NotaService notaService;
 
     @Operation(summary = "Retorna notas de aluno pelo ID", description = "Retorna todas as notas de um aluno, localizando-as por seu ID")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna somente o Status Code da requisição HTTP"),
-                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
-                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
-            }
-    )
+    @Response
     @GetMapping("/{idAluno}")
     public ResponseEntity<List<NotaDTO>> getByIdAluno(@PathVariable Integer idAluno) throws RegraDeNegocioException {
         return new ResponseEntity<>(notaService.listByIdAluno(idAluno), HttpStatus.OK);
     }
 
     @Operation(summary = "Atualiza nota", description = "Atualiza notas, localizando-as por seu ID")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "200", description = "Retorna somente o Status Code da requisição HTTP"),
-                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
-                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
-            }
-    )
+    @Response
     @PutMapping("/{idNota}")
     public ResponseEntity<NotaDTO> putNota(@PathVariable Integer idNota, @Valid @RequestBody NotaCreateDTO notaCreateDTO) throws RegraDeNegocioException {
         return new ResponseEntity<>(notaService.atualizarNotasAluno(idNota, notaCreateDTO), HttpStatus.OK);
