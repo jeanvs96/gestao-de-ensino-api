9140eebbe2e2de1f9ba518c0d8a7aa9493e54425
Jean <jean.silva@dbccompany.com.br>
2022-07-22 00:39:12 -0300

    Mapeamento de NotaEntity

--- src/main/java/br/com/dbccompany/time7/gestaodeensino/entity/NotaEntity.java
+++ src/main/java/br/com/dbccompany/time7/gestaodeensino/entity/NotaEntity.java
@@ -3,26 +3,33 @@ package br.com.dbccompany.time7.gestaodeensino.entity;
 
 import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "notas")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class NotaEntity {
 
+    @Id
+    @Column(name = "id_notas")
     private Integer idNota;
 
-    private Integer idDisciplina;
-
-    private Integer idAluno;
-
+    @Column(name = "n1")
     private Double nota1;
 
+    @Column(name = "n2")
     private Double nota2;
 
+    @Column(name = "n3")
     private Double nota3;
 
+    @Column(name = "n4")
     private Double nota4;
 
+    @Column(name = "media")
     private Double media;
 }
