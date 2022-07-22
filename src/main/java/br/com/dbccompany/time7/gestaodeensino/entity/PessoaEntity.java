f9ba222b0c8e2bda21f13a82b7e0dbe96b65681b
Jean <jean.silva@dbccompany.com.br>
2022-07-22 00:20:05 -0300

    Mapeamento de PessoaEntity e ProfessorEntity

--- src/main/java/br/com/dbccompany/time7/gestaodeensino/entity/PessoaEntity.java
+++ src/main/java/br/com/dbccompany/time7/gestaodeensino/entity/PessoaEntity.java
@@ -1,12 +1,26 @@
 package br.com.dbccompany.time7.gestaodeensino.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PessoaEntity {
 
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "telefone")
     private String telefone;
+
+    @Column(name = "email")
     private String email;
-    private Integer idEndereco;
 }
--- src/main/java/br/com/dbccompany/time7/gestaodeensino/entity/ProfessorEntity.java
+++ src/main/java/br/com/dbccompany/time7/gestaodeensino/entity/ProfessorEntity.java
@@ -1,23 +1,45 @@
 package br.com.dbccompany.time7.gestaodeensino.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Set;
 
-@Entity
+@Entity(name = "professor")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProfessorEntity {
+public class ProfessorEntity extends PessoaEntity{
 
+    @Id
+    @SequenceGenerator(name = "SEQ_PROFESSOR", sequenceName = "seq_professor", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PROFESSOR")
+    @Column(name = "id_professor")
     private Integer idProfessor;
-    private String nome;
-    private String telefone;
-    private String email;
-    private Integer idEndereco;
+
+    @SequenceGenerator(name = "SEQ_REGISTRO_TRABALHO", sequenceName = "seq_registro_trabalho", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_REGISTRO_TRABALHO")
+    @Column(name = "registro_trabalho")
     private Integer registroTrabalho;
+
+    @Column(name = "salario")
     private Double salario;
 
+    @Column(name = "cargo")
     private String cargo;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY,
+            cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id_endereco", referencedColumnName = "id_endereco")
+    private EnderecoEntity enderecoEntity;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "professorEntity",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.MERGE)
+    private Set<DisciplinaEntity> disciplinaEntities;
 }
