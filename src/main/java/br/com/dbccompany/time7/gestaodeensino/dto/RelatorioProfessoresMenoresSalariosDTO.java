93ad6cde07a835ede24db138652daf9024a9f487
Eduardo Miranda <eduardo.cmazevedo@gmail.com>
2022-07-22 17:32:57 -0300

    Dtos de relatório criados.

---
+++ src/main/java/br/com/dbccompany/time7/gestaodeensino/dto/RelatorioProfessoresMenoresSalariosDTO.java
@@ -0,0 +1,19 @@
+package br.com.dbccompany.time7.gestaodeensino.dto;
+
+
+import br.com.dbccompany.time7.gestaodeensino.entity.DisciplinaEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RelatorioProfessoresMenoresSalariosDTO {
+    private Integer registroTrabalho;
+    private String nomeProfessor;
+    private Set<DisciplinaEntity> disciplinasEntities;
+    private Double salario;
+}
