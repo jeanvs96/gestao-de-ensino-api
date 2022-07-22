fbc458c00e9e9d26e2b4fb104e3b7e488189e7cf
Jean <jean.silva@dbccompany.com.br>
2022-07-22 14:59:52 -0300

    Fix Sequence

--- DB/create.sql
+++ DB/create.sql
@@ -1,26 +1,29 @@
-create schema gestao_ensino_api
 
-    CREATE TABLE ENDERECO (
-                              ID_ENDERECO bigint NOT NULL,
-                              LOGRADOURO text NOT NULL,
-                              NUMERO bigint NOT NULL,
-                              COMPLEMENTO text,
-                              CIDADE text NOT NULL,
-                              ESTADO text NOT NULL,
-                              CEP text NOT NULL,
-                              PRIMARY KEY (ID_ENDERECO)
-    );
+CREATE TABLE ENDERECO (
+                          ID_ENDERECO bigint NOT NULL,
+                          LOGRADOURO text NOT NULL,
+                          NUMERO bigint NOT NULL,
+                          COMPLEMENTO text,
+                          CIDADE text NOT NULL,
+                          ESTADO text NOT NULL,
+                          CEP text NOT NULL,
+                          PRIMARY KEY (ID_ENDERECO)
+);
 
 CREATE SEQUENCE SEQ_ENDERECO
     INCREMENT 1
 START 1;
+
+CREATE SEQUENCE SEQ_REGISTRO_TRABALHO
+    INCREMENT 1
+START 1000;
 
 CREATE TABLE PROFESSOR (
                            ID_PROFESSOR bigint NOT NULL,
                            NOME text NOT NULL,
                            TELEFONE text NOT NULL,
                            EMAIL text NOT NULL,
-                           REGISTRO_TRABALHO bigint NOT NULL,
+                           REGISTRO_TRABALHO bigint NOT NULL DEFAULT nextval('gestao_ensino_api.seq_registro_trabalho'),
                            CARGO text NOT NULL,
                            SALARIO DECIMAL(7,2) NOT NULL,
                            ID_ENDERECO bigint,
@@ -33,10 +36,6 @@ CREATE TABLE PROFESSOR (
 CREATE SEQUENCE SEQ_PROFESSOR
     INCREMENT 1
 START 1;
-
-CREATE SEQUENCE SEQ_REGISTRO_TRABALHO
-    INCREMENT 1
-START 1000;
 
 CREATE TABLE DISCIPLINA (
                             ID_DISCIPLINA bigint NOT NULL,
@@ -62,12 +61,18 @@ CREATE SEQUENCE SEQ_CURSO
     INCREMENT 1
 START 1;
 
+
+CREATE SEQUENCE SEQ_ALUNO_MATRICULA
+    INCREMENT 1
+START 1000;
+
+
 CREATE TABLE ALUNO (
                        ID_ALUNO bigint NOT NULL,
                        NOME text NOT NULL,
                        TELEFONE text NOT NULL,
                        EMAIL text NOT NULL,
-                       MATRICULA bigint NOT NULL,
+                       MATRICULA bigint NOT NULL DEFAULT nextval('gestao_ensino_api.seq_aluno_matricula'),
                        ID_CURSO bigint,
                        ID_ENDERECO bigint,
                        PRIMARY KEY (ID_ALUNO),
@@ -83,9 +88,6 @@ CREATE SEQUENCE SEQ_ALUNO
     INCREMENT 1
 START 1;
 
-CREATE SEQUENCE SEQ_ALUNO_MATRICULA
-    INCREMENT 1
-START 1000;
 
 CREATE TABLE NOTAS (
                        ID_NOTAS bigint NOT NULL,
@@ -105,6 +107,7 @@ CREATE TABLE NOTAS (
                                REFERENCES DISCIPLINA(ID_DISCIPLINA)
 );
 
+
 CREATE SEQUENCE SEQ_NOTAS
     INCREMENT 1
 START 1;
@@ -122,6 +125,7 @@ CREATE TABLE DISCIPLINA_X_CURSO (
                                             REFERENCES DISCIPLINA(ID_DISCIPLINA)
 );
 
+
 CREATE SEQUENCE SEQ_DISCIPLINA_X_CURSO
     INCREMENT 1
 START 1;
@@ -132,7 +136,7 @@ VALUES (nextval('SEQ_ENDERECO'), 'Rua das Ruas', 8, 'Apartament 30', 'São Paulo
 INSERT INTO GESTAO_ENSINO_API.ENDERECO (ID_ENDERECO, LOGRADOURO, NUMERO, COMPLEMENTO, CIDADE, ESTADO, CEP)
 VALUES (nextval('SEQ_ENDERECO'), 'Rua das Máquinas', 22, NULL, 'São Paulo', 'São Paulo','05586-325');
 
-INSERT INTO GESTAO_ENSINO_API.PROFESSOR (ID_PROFESSOR, NOME, TELEFONE, EMAIL, REGISTRO_TRABALHO, CARGO, SALÁRIO, ID_ENDERECO)
+INSERT INTO GESTAO_ENSINO_API.PROFESSOR (ID_PROFESSOR, NOME, TELEFONE, EMAIL, REGISTRO_TRABALHO, CARGO, SALARIO, ID_ENDERECO)
 VALUES (nextval('SEQ_PROFESSOR'), 'José Antônio', '5511999995555', 'jose.antonio@gmail.com', nextval('SEQ_REGISTRO_TRABALHO'), 'Professor', 5500, 1);
 
 INSERT INTO GESTAO_ENSINO_API.CURSO (ID_CURSO, NOME)
@@ -147,21 +151,5 @@ VALUES (nextval('SEQ_ALUNO'), 'Fernando Fernandes', '5522888883333', 'fernando.f
 INSERT INTO GESTAO_ENSINO_API.NOTAS (ID_NOTAS, N1, N2, N3, N4, MEDIA, ID_DISCIPLINA, ID_ALUNO)
 VALUES (nextval('SEQ_NOTAS'), NULL, NULL, NULL, NULL, NULL, 1, 1)
 
-    INSERT INTO GESTAO_ENSINO_API.DISCIPLINA_X_CURSO (ID_DISCIPLINA_X_CURSO, ID_DISCIPLINA, ID_CURSO)
+INSERT INTO GESTAO_ENSINO_API.DISCIPLINA_X_CURSO (ID_DISCIPLINA_X_CURSO, ID_DISCIPLINA, ID_CURSO)
 VALUES (nextval('SEQ_DISCIPLINA_X_CURSO'), 1, 1);
-
-SELECT * FROM CURSO;
-SELECT * FROM ENDERECO;
-SELECT * FROM ALUNO;
-
-
-
-DROP TABLE DISCIPLINA_X_CURSO;
-DROP TABLE NOTAS;
-DROP TABLE ALUNO;
-DROP TABLE DISCIPLINA;
-DROP TABLE CURSO;
-DROP TABLE PROFESSOR;
-DROP TABLE ENDERECO;
-
-TRUNCATE TABLE ENDERECO;
---
+++ DB/dropall.sql
@@ -0,0 +1,3 @@
+drop table if exists disciplina_x_curso , notas , aluno , disciplina , professor , endereco, curso
+
+DROP SEQUENCE IF EXISTS seq_disciplina_x_curso , seq_notas , seq_aluno , seq_aluno_matricula , seq_disciplina, seq_disciplina_x_curso , seq_professor, seq_registro_trabalho , seq_endereco , seq_curso cascade
